6a342737f6a00ff7d2437431f1c3715bf5259d86
Morten Christensen <mortenemdec@gmail.com>
2020-04-21 15:08:50 +0200

    add react native elements lib

---
+++ client/components/JourneyPlanner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+const JourneyPlanner = () => {
+  return (
+    <View style={styles.container}>
+      <TextInput></TextInput>
+      <Text>Open up to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default JourneyPlanner
