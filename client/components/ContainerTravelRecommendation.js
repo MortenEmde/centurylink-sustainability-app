7e9f14ef930ed848ffa6b813931756c526452f2c
zimmah <zimmah@gmail.com>
2020-04-23 10:23:12 +0200

    use hooks

--- client/components/ContainerTravelRecommendation.js
+++ client/components/ContainerTravelRecommendation.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import CardTravelRecommendation from './CardTravelRecommendation';
 
 const ContainerTravelRecommendation = async () => {
-// use hooks to avoid infinite loop?
-  const unparsedData = await fetch('http://localhost:5000/api', {method: 'POST'}) 
-  const data = await unparsedData.json();
-  console.log(data);
+  const [journeyData, setJourneyData] = useState([]);
+  const fetchData = async () => {
+    const unparsedData = await fetch('http://localhost:5000/api', {method: 'POST'}) 
+    const data = await unparsedData.json();
+    console.log('fetcheddata', data)
+    setJourneyData(data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [journeyData]);
+
+  console.log(journeyData);
+
+  // const endResult = async () => {
+  //   return await Promise.all(data.map((item, index) => 
+  //     <CardTravelRecommendation key={index} journeyInfo={item.time}/>))
+  // }
 
   const endResult = async () => {
-    return Promise.all(data.map((item, index) => 
-    <CardTravelRecommendation key={index} journeyInfo={item.time}/>))
+    return <CardTravelRecommendation journeyInfo={journeyData[0]}/>
   }
+
   return (
     <View>
+      {console.log('endResult', await endResult())}
       {await endResult()}
     </View>
   )
