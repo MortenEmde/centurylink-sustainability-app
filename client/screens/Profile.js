f6fb6dcdea252737184363012561f86bc584ba61
nicholsonhelena <nicholson.helena@gmail.com>
2020-04-27 15:18:37 +0200

    profile and scoreboard

--- client/screens/Profile.js
+++ client/screens/Profile.js
@@ -1,14 +1,78 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Avatar } from 'react-native-elements';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faBicycle, faWalking, faCarSide, faShuttleVan, faLeaf, faHeartbeat, faClock } from '@fortawesome/free-solid-svg-icons';
 
 const Profile = () => {
   return (
     <View>
-      <Text>Hello Profile</Text>
+      <View style={styles.avatar}>
+        <Avatar 
+          size={200}
+          rounded
+          source={{
+            uri:
+              'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+          }}
+          showEditButton
+        />
+      </View>
+      <View style={styles.userInfo}>
+        <Text 
+          style={styles.name}>
+          Jane Doe
+        </Text>
+        <Text 
+          style={styles.department}>
+          Department: Technology
+        </Text>
+      </View>
+      <View style={styles.pointsRow}>
+        <FontAwesomeIcon
+          icon={faHeartbeat}
+          style={styles.heartIcon}
+          size={30}
+        />
+        <Text style={styles.pointsText}>
+          {10}
+        </Text>
+      </View>
+      <View style={styles.pointsRow}>
+        <FontAwesomeIcon
+          icon={faLeaf}
+          style={styles.leafIcon}
+          size={30}
+        />
+        <Text style={styles.pointsText}>
+          {30}
+        </Text>
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  avatar: {
+    alignItems: 'center',
+  },
+  userInfo: {
+    alignItems: 'center',
+  },
+  name: {
+    fontSize: 50,
+  },
+  department: {
+    fontSize: 20,
+  },
+  pointsRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  pointsText: {
+    fontSize: 30,
+  }
+})
 
 export default Profile;
--- client/screens/Scoreboard.js
+++ client/screens/Scoreboard.js
@@ -1,14 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Card, ListItem, Button, Icon } from 'react-native-elements';
+// Divider: https://react-native-elements.github.io/react-native-elements/docs/divider.html
+
+const users = [
+  {
+    name: 'brynn',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
+  },
+  {
+    name: 'brynn',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
+  },
+  {
+    name: 'brynn',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
+  },
+  {
+    name: 'brynn',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
+  },
+];
 
 const Scoreboard = () => {
   return (
     <View>
-      <Text>Hello Scoreboard</Text>
+      <Card containerStyle={{ padding: 0 }} title="CARD WITH DIVIDER">
+        {users.map((u, i) => {
+          return (
+            <ListItem
+              key={i}
+              roundAvatar
+              title={u.name}
+              avatar={{ uri: u.avatar }}
+            />
+          );
+        })}
+      </Card>
     </View>
   );
 };
 
 const styles = StyleSheet.create({});
 
-export default Scoreboard
+export default Scoreboard;
