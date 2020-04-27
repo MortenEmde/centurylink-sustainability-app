14684a4ca71304dba1493e2ba82f8e8884ee8216
laurendorothy <ldchallis@gmail.com>
2020-04-27 12:01:52 +0200

    add icons

--- client/screens/drivingpref.js
+++ client/screens/drivingpref.js
@@ -1,14 +1,75 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCarSide, faUsers, faUtensils, faToolbox, faChair, faDesktop } from '@fortawesome/free-solid-svg-icons';
+import { CheckBox, Button } from 'react-native-elements'
 
 const Drivingpref = () => {
   return (
     <View>
-      <Text>Hello Drivingpref</Text>
+    <View style={styles.employeeCount}>
+      <Text>Current employees in office: 24</Text>
+      <Text>Current employees in your department: 6</Text>
+    </View>
+    <View style={styles.option}>
+      <FontAwesomeIcon
+                icon={faCarSide}
+                size={60}
+              />
+      <FontAwesomeIcon
+                icon={faUsers}
+                size={60}
+              />
+      <CheckBox
+        // add state
+        // checked={this.state.checked}
+        // onPress={() => this.setState({checked: !this.state.checked})}
+      />
+    </View>
+    <View style={styles.option}>
+       <FontAwesomeIcon
+              icon={faUtensils}
+              size={60}
+            />
+      <FontAwesomeIcon
+            icon={faToolbox}
+            size={60}
+            />
+      <CheckBox
+        // add state
+        // checked={this.state.checked}
+        // onPress={() => this.setState({checked: !this.state.checked})}
+      />
+    </View>
+    <View style={styles.option}>
+       <FontAwesomeIcon
+              icon={faDesktop}
+              size={60}
+            />
+      <FontAwesomeIcon
+            icon={faChair}
+            size={60}
+            />
+      <CheckBox
+        // add state
+        // checked={this.state.checked}
+        // onPress={() => this.setState({checked: !this.state.checked})}
+      />
+    </View>
+    <Button title="Start journey" />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  employeeCount: {
+    alignItems: "center",
+    marginTop: 15
+
+  },
+  option: {
+    flexDirection: "row",
+    margin: 20
+  } });
 
 export default Drivingpref;
